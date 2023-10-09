426514696e7d34f5ff92e12e6cb4906b7e999605
vijayakumar58 <vijayalingam58@gmail.com>
2023-10-09 12:29:39 +0530

    add admin

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.listen(process.env.PORT || 3000);
 app.use(express.json());
 app.use(cors({
     origin : "https://crmfrontend.netlify.app"
+    // origin : "http://localhost:3001"
 }))
 
 const Authenticate = (req,res,next) => {
@@ -130,7 +131,133 @@ app.post('/login',async function(req,res){
         if (user) {
             const compare = await bcrypt.compare(req.body.password, user.password);
             if (compare) {
-                const token = jwt.sign({_id: user._id},process.env.SECRET,{expiresIn:"30m"})
+                const token = jwt.sign({_id: user._id},process.env.SECRET,{expiresIn:"10m"})
+                res.json({token})
+            } else {
+                console.log(error)
+                res.json({message:"Enter correct Password"})
+            }
+        } else {
+            console.log(error)
+            res.status(401).json({message:"Enter correct Email"})
+        }
+        await connection.close();
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Something Went Wrong"})
+    }
+})
+
+//create Admin 
+app.post('/admin',Authenticate, async function(req,res){
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db(DB);
+        await db.collection('admins').insertOne(req.body);
+        await connection.close();
+        res.json({message:"Admin Data Insert"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Something Went Wrong"})
+    }
+})
+
+//get admins
+app.get('/admins',Authenticate, async function(req,res){
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db(DB);
+        const allAdmin = await db.collection('admins').find().toArray();
+        await connection.close();
+        res.json(allAdmin);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Something Went Wrong"})
+    }
+})
+
+//get users in admin account
+app.get('/admins',Authenticate, async function(req,res){
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db(DB);
+        const allusersinAdmin = await db.collection('users').find().toArray();
+        await connection.close();
+        res.json(allusersinAdmin);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Something Went Wrong"})
+    }
+})
+
+//view Admin
+app.get('/admin/:id',Authenticate, async function(req,res){
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db(DB);
+        const admin = await db.collection('admins').findOne({ _id: new mongodb.ObjectId(req.params.id) });
+        await connection.close();
+        res.json(admin);
+    } catch (error) {
+        res.status(500).json({message:"Something Went Wrong"})
+    }
+})
+
+//Edit admin
+app.put('/admin/:id',Authenticate, async function(req,res){
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db(DB);
+        const viewAdmin = await db.collection('admins').findOneAndUpdate({_id: new mongodb.ObjectId(req.params.id)},{$set:req.body});
+        await connection.close();
+        res.json(viewAdmin);
+    } catch (error) {
+        res.status(500).json({message:"Something Went Wrong"})
+    }
+})
+
+//Delete admin
+app.delete('/admin/:id',Authenticate, async function(req,res){
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db(DB);
+        const admin = await db.collection('admins').findOneAndDelete({ _id: new mongodb.ObjectId(req.params.id)});
+        await connection.close();
+        res.json(admin);
+    } catch (error) {
+        res.status(500).json({message:"Something Went Wrong"})
+    }
+})
+
+//create Adminregister
+app.post('/createadminregister',async function(req,res){
+    try{
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db(DB);
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password,salt);
+        req.body.password = hash;
+        req.body.repeatpassword = hash;
+        await db.collection('adminregister').insertOne(req.body);
+        await connection.close();
+        res.json({message:"Admin insert successfully"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"something went wrong"});
+    }
+})
+
+//Admin login 
+app.post('/adminlogin',async function(req,res){
+    try {
+        const connection = await mongoClient.connect(URL);
+        const db = connection.db(DB);
+        const admin = await db.collection('adminregister').findOne({ email: req.body.email });
+        if (admin) {
+            const compare = await bcrypt.compare(req.body.password, admin.password);
+            if (compare) {
+                const token = jwt.sign({_id: admin._id},process.env.SECRET,{expiresIn:"10m"})
                 res.json({token})
             } else {
                 console.log(error)
